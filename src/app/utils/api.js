4923bdfb54ee80bbcc28bc5d996452beb5139023
andrija14 <andrija.zecevic098@gmail.com>
2025-10-09 22:00:54 +0200

    frontend92

--- src/app/utils/api.js
+++ src/app/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000',
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://dijalog.undovrbas.com',
 });
 
 api.interceptors.request.use(
